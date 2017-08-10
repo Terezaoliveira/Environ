87cd212013be3ae2d568757323b5be63859b3500
lmedson <lmedson@users.noreply.github.com>
2017-08-10 15:30:33 -0300

    #0X.1 fixing condition

--- sendmail.php
+++ sendmail.php
@@ -1,10 +1,15 @@
 <?php
-if($_POST){
+if(!is_null($_POST) ){
 	$header = "From: Site Environmakers";
-    $user = "rafaeldantas@ejectufrn.com.br";
-    $title = "[SITE] Novo Contato - ".$_POST['Subject'];
-    $msg = "Mensagem: \n" .$_POST['message']. "\n\nEnviado por: ".$_POST['name']." | " .$_POST['email']. "\nContato: ".$_POST['phone'] "\nWebsite: ".$_POST['website'] "\nLocal: ".$_POST['address'];
-    echo mail($user, $title, $msg); // 1 ok / 0 fail
+	$header = "Content-type: text/html\r\n";
+	$user = "barbaravianello@ejectufrn.com.br";//poe o email aqui
+	$title = "[SITE] Novo Contato - ".$_POST['motivo'];
+	$msg = "Mensagem: \n".$_POST['mensagem']."\n\nEnviado por: ".$_POST['nome']." | ".$_POST['e-mail']."\nWebsite: ".$_POST['site']."\nLocal: ".$_POST['local'];
+	 mail($user, $title, $msg); // 1 ok / 0 fail
 } else
     header('Location: ./');
+//OBS.: nas variáveis dentro de _POST, tem que ser dado o name dos inputs, se passar o o id vai enviar vazio
 ?>
+
+
+
